cbc2831f30d79a4a75703c5193791e461bc72393
Rjenkins76 <jenkinsrogerrabbit@yahoo.com>
2024-08-10 08:32:10 -0500

    UPDATE

--- Levels2.py
+++ Levels2.py
@@ -92,7 +92,300 @@ def DrawStartPosition(Level):
         Rooms_MAP.CELL_DRAW[_PositionCell][1] = Rooms_MAP.CELLS[3][1]
         Rooms_MAP.CELL_DRAW[_PositionCell][2] = Rooms_MAP.CELLS[3][2]
 
+### GET 25% OF THE TIME PLAYER FALLS INTO A TRAP ###
+def Percentage():
+    if random.random() * 100 < 25:
+        return True
+    return False
+### DISPLAY PLAYER FALLS INTO TRAP AND GET IF PLAYER HEALTH REDUCED ###
+def EncounterTrap():
+    trap = Percentage()
+    if trap:
+        DrawHeader()
+        print("\n" * 4 + fg(250,100,100))
+        for line in TrapText:
+            print(line.center(208, " "))
+        print("\n" * 3 + fg(255,255,50))
+        print(" You fell into a trap....".center(208, " "))
+        print(" Are you strong enough to escape, roll the die to find out".center(208, " "))
+        print(" Roll a 1,2,3, or 4 - and you escape unharmed".center(208, " "))
+        print(" Roll a 5,6,7,8 or 9 - and escape with injuries".center(208, " "))
 
+        result = DiceMove2()  ### ROLL DICE TO SEE IF PAYER WILL LOSE HP - 9 SIDED DICE
+        Banner.Clear_Line(1)
+        if (result == 1) or (result == 2) or (result == 3) or (result == 4): # 9 SIDED DICE - PLAYER ROLL < 5
+            print(fg(100, 250, 100))
+            print(f"{Player.PlayerInfo.PlayerName} you escape unharmed!".center(208, " "))
+            input("PRESS ENTER TO CONTINUE".rjust(115))
+        elif (result == 5) or (result == 6) or (result == 7) or (result == 8) or (result == 9): # 9 SIDED DICE  - PLAYER ROLL > 4
+            HPloss = random.randint(1, int(Player.PlayerInfo.PlayerCurrentHealth / 5)) # PLAYER CAN ONLY LOSE 1/5th OF HEALTH AT A TIME FOR TRAP
+            Player.PlayerInfo.PlayerCurrentHealth -= HPloss
+            print(fg(255,25,25))
+            print(f"You have lost {HPloss} HP and your current HP is {Player.PlayerInfo.PlayerCurrentHealth}.".center(208, " "))
+            input("PRESS ENTER TO CONTINUE".rjust(115))
+            print(fg.rs)
+
+### IF CELL CONTAINS A GHOST DRAW GHOST AND GIVE OPTION TO FEED OR FIGHT
+def EnounterGhost(move):
+    story = ""
+    name = ""
+    Hp = 0
+    ghost_info = []
+    ### SELECT CORRECT GHOST AND GET IT'S INFORMATION
+    for ghost in Ghost.Ghost_List:
+        if ghost.location == move[0]:
+            story = ghost.story
+            name = ghost.name
+            Hp = ghost.Ghost_HP
+            ghost_info.append([name, story, Hp])
+
+            ### DRAW GHOST AND STORY
+    ### GIVE OPTION TO FIGHT OR FEED
+    x, z = 0, 0  ### X = GHOST LINES, Z = STORY LINES
+    story_length = len(story)
+    Ghost_length = len(GhostASCII.printBasicGhost)
+    where_to_Start = int(
+        Ghost_length / story_length)  ### GET LINE TO START GHOST INFORMATION IN REFERENCE TO THE GHOST ASCII
+    for i in GhostASCII.printBasicGhost:
+        if z == 1:
+            print(i.rjust(45) + f" YOU HAVE ENCOUNTERED THE GHOST OF {name}".center(150, " "))
+            z += 1
+        elif z == 2:
+            print(i.rjust(45) + f" HEALTH: {Hp}".center(150, " "))
+            z += 1
+        elif (z == where_to_Start) and (x < story_length):
+            print(i.rjust(45) + story[x].center(150, " "))
+            x += 1
+            where_to_Start += 1
+            z += 1
+        elif z == 10:
+            print(i.rjust(45) + fg(255, 255, 0) + "1. FEED GHOST".center(150, " ") + fg.rs)
+            z += 1
+        elif z == 12:
+            print(i.rjust(45) + fg(255, 255, 0) + "2. FIGHT GHOST".center(150, " ") + fg.rs)
+            z += 1
+        else:
+            print(i.rjust(45))
+            z += 1
+
+            # print (f"\033[38;5;{227}m")
+    Banner.Clear_Line()
+    option = input("SELECT OPTION: ".rjust(125))
+    if option == "1":
+        FeedingGhost() ### CALL TO FEED THE GHOST
+    elif option == "2":
+        FightingGhost(ghost_info)  ### CALL GHOST FIGHT SCRIPT
+    else:
+        EnounterGhost(move)  ### RESTART IF NOT ONE OF THE OPTIONS
+
+### DISPLAY GHOST FIGHT
+def FightingGhost(ghost_info):
+    Banner.Clear_Line(int(12 * 3.9))
+    # region DRAW WEAPONS AVAILABLE
+    print("\n" * 15 + fg(255, 255, 0))
+    print("❚█══SELECT WEAPON══█❚".center(208, " "))
+    print("\n\n")
+    x = 1
+    for W in Player.PlayerInfo.PlayerWeapons:
+        print((str(x) + ". " + str(W[1]) + " - " + str(W[0]) + " - " + str(W[2]) + " DAMAGE").center(208, " "))
+        print("\n")
+        x += 1
+    item = input("SELECT A WEAPON TO USE: ".rjust(115))
+    # endregion
+
+    # region DRAW FIGHTING SEQUENCE
+    Banner.Clear_Line(int(12 * 3.9))
+    print("\n" * 30)
+    color = 255
+    while color > -5:
+        Banner.Clear_Line(len(GhostASCII.printFight) + 1)
+        print(fg.rgb_call(color, color, color))
+        for i in GhostASCII.printFight:
+            print(i.center(208, " "))
+
+        time.sleep(0.1)
+        color -= 17
+
+    color = 0
+    Banner.Clear_Line(len(GhostASCII.printFight) + 1)
+    print("\n" * (30 - len(GhostASCII.printFight)))
+    while color < 255:
+        Banner.Clear_Line(len(GhostASCII.printFight2) + 1)
+        print(fg.rgb_call(color, color, color))
+        for i in GhostASCII.printFight2:
+            print(i.center(208, " "))
+
+        time.sleep(0.1)
+        color += 17
+
+    color = 255
+    Banner.Clear_Line(len(GhostASCII.printFight2))
+    print("\n" * (30 - len(GhostASCII.printFight2)))
+    while color > -5:
+        Banner.Clear_Line(len(GhostASCII.printFight) + 1)
+        print(fg.rgb_call(color, color, color))
+        for i in GhostASCII.printFight:
+            print(i.center(208, " "))
+
+        time.sleep(0.1)
+        color -= 17
+
+    # endregion
+
+    # region DISPLAY FIGHT RESULTS
+    print("\n" * 2 + fg(255, 255, 0))
+    Ghost_Status = Percentage()
+    if Ghost_Status:
+        print(fg(255, 255, 0) + "YOU HAVE BATTLED WITH THE GHOST....".center(208, " "))
+        print(fg(255, 255, 0) + f"YOU HAVE DEFEATED THE GHOST !!!!!!!".center(208, " "))
+        Player.PlayerInfo.AddHealth(random.randint(15, 45))
+        print(fg(255, 255,
+                 0) + f"YOU HAVE GAINED HEALTH AND YOUR CURRENT HELTH IS: {Player.PlayerInfo.PlayerCurrentHealth}".center(
+            208, " ") + "\n\n")
+        input("PRESS ANY KEY TO CONTINUE...".rjust(120))
+    else:
+        print(fg(255, 255, 0) + "YOU HAVE BATTLED WITH THE GHOST.....".center(208, " "))
+        ghost_info[0][2] -= random.randint(0, 50)
+        print(fg(255, 255, 0) + f"YOU HAVE REDUCED THEIR HEALTH TO {ghost_info[0][2]}".center(208, " "))
+        Player.PlayerInfo.RemoveHealth(random.randint(0, 35))
+        print(fg(255, 255,
+                 0) + f"YOU HAVE LOST HEALTH AND YOUR CURRENT HELTH IS: {Player.PlayerInfo.PlayerCurrentHealth}".center(
+            208, " ") + "\n\n")
+        input("PRESS ANY KEY TO CONTINUE...".rjust(120))
+
+    # endregion
+
+### DISPLAY FEEDING GHOST - FIXME - NEED TO WORK ON HP PORTION
+def FeedingGhost():
+    Banner.ClearScreen()
+    for i in GhostASCII.printGhostEating:
+        print(i.center(208, " "))
+    input()
+
+### COLLECTS PLAYER INPUT FOR SELECTED MOVES EACH TURN ###
+### PLAYER CAN ALSO VIEW THEIR FOOD ITEMS AND WEAPONS  ###
+def PlayerMove(num_Moves):
+    global _PositionCell,Moves_Made
+    move = 0
+    while move < num_Moves:
+        Banner.ChangeFOREcolor(255,255,0)
+        Banner.Clear_Line()
+        MoveSelection = input(f"You have {num_Moves - move} moves left (8: UP,  4: LEFT, 2: DOWN, 6: RIGHT️): ".rjust(130, " "))
+        if MoveSelection == "8":
+            move += 1
+            MovePlayer("UP")
+
+        elif MoveSelection == "2":
+            move += 1
+            MovePlayer("DOWN")
+
+        elif MoveSelection == "4":
+            move += 1
+            MovePlayer("LEFT")
+
+        elif MoveSelection == "6":
+            move += 1
+            MovePlayer("RIGHT")
+
+        elif (MoveSelection.upper() == "F"):
+            print(f"LISTING YOUR FOOD ITEMS: {(Player.PlayerInfo.PlayerFood)}".center(208, " "))
+        elif (MoveSelection.upper() == "W"):
+            print(f"LISTING YOUR WEAPONS: {(Player.PlayerInfo.PlayerFood)}".center(208, " "))
+
+        Banner.EndColor()
+        Banner.Clear_Line()
+
+### CHECK TO SEE IF PLAYER ENTERS ROOM OR ITEM IN EACH CELL PLAYER WAS IN ###
+def Blockedcell():
+    global Moves_Made, Game_Level
+    fellintotrap = False
+    ### IF PLAYER ENTERS A ROOM - ***FIXME***
+    if Game_Level == 1:
+        rooms = Floor1_Map.Room_List
+    elif Game_Level == 2:
+        rooms = Floor2_Map.Room_List
+
+    for room in rooms:
+        if _PositionCell == room:
+            if Game_Level == 1:
+                Game_Level = 3
+            elif Game_Level == 2:
+                Game_Level = 4
+
+        elif _PositionCell == 13:
+            Game_Level = 2
+
+    else:
+        for move in Moves_Made:
+            ### IF A CELL CONTAINED A FOOD ITEM - ***FIXME***
+            if (move[1] == '  🥖  '):
+                Banner.Clear_Line(int(12 * 3.9) + 12)
+                print("\n" * 15)
+                for i in FoodASCII.GenericFood:
+                    print("".rjust(45) + i)
+
+                print((fg(255, 255, 0) + "                       1. EAT" + fg.rs).rjust(130))
+                print((fg(255, 255, 0) + "                       2. SAVE FOR LATER" + fg.rs).rjust(130))
+                option = input("Select Option: ".rjust(100))
+                print()
+                if option == "1":
+                    FeedingGhost()
+                    Player.PlayerInfo.AddHealth(10)
+                    input()
+                elif option == "2":
+                    print("PUTTING THIS IN BACKPACK")
+                    Player.PlayerInfo.AddHealth(3)
+            ### IF A CELL CONTAINED A GHOST - PLAYER SELECTS TO FEED OR FIGHT
+            elif (move[1] == '  👻  '):
+                Banner.Clear_Line(int(12 * 3.9) + 12)
+                # SlidingGhost()
+                Banner.Clear_Line(4)
+                print("\n")
+                EnounterGhost(move)
+                pass
+            ### IF A CELL CONTAINED A WEAPON - ***FIXME***
+            elif (move[1] == '   ⚔️ '):
+                pass
+            else: ### DOES PLAYER FIND A TRAP?
+                if not fellintotrap:
+                    EncounterTrap()
+                    fellintotrap = True
+
+
+
+    Moves_Made.clear()  ### CLEAR LIST FOR NEXT PLAYER TURN
+    
+### MOVES PLAYER BASED ON SELECTED DIRECTION ###
+def MovePlayer(direction):
+    global _PositionCell,Moves_Made
+    if Game_Level == 1:
+        CELL_DRAW = Floor1_Map.CELL_DRAW
+        CELLS = Floor1_Map.CELLS
+    if Game_Level == 2:
+        CELL_DRAW = Floor2_Map.CELL_DRAW
+        CELLS = Floor2_Map.CELLS
+    if Game_Level == 3:
+        CELL_DRAW = Rooms_MAP.CELL_DRAW
+        CELLS = Rooms_MAP.CELLS
+    if Game_Level == 4:
+        CELL_DRAW = Rooms_MAP.CELL_DRAW
+        CELLS = Rooms_MAP.CELLS
+
+    CELL_DRAW[_PositionCell] = [CELLS[1][0], CELLS[1][1], CELLS[1][2]]
+
+    if direction == "UP":
+        _PositionCell -= 28
+    elif direction == "DOWN":
+        _PositionCell += 28
+    elif direction == "LEFT":
+        _PositionCell -= 1
+    elif direction == "RIGHT":
+        _PositionCell += 1
+
+    Moves_Made.append([_PositionCell, CELL_DRAW[_PositionCell][1]])
+    CELL_DRAW[_PositionCell] = [CELLS[11][0], CELLS[11][1], CELLS[11][2]]
+    Banner.Clear_Line()
+    time.sleep(0.3)
 
 
 
