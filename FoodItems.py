5d9fedb862403b9f6b823ce6622f430bf32487ff
Rjenkins76 <jenkinsrogerrabbit@yahoo.com>
2024-08-12 13:32:18 -0500

    UPDATE

--- FoodItems.py
+++ FoodItems.py
@@ -15,16 +15,16 @@ from FoodASCII import *
 # Food_ASCII_Art
 ###################################
 FoodItemList = []
-FoodItemList.append(['CAKE','🍰',0,25,7,Cake])
-FoodItemList.append(['Burger','🍔',0,30,10,Burger])
-FoodItemList.append(['Fries','🍟',0,20,10,GenericFood])
-FoodItemList.append(['BACON','🥓',0,40,15,Bacon])
+FoodItemList.append(['SLICE OF CAKE','🍰',0,25,7,Cake])
+FoodItemList.append(['CHEESE BURGER','🍔',0,30,10,Burger])
+FoodItemList.append(['CONTAINER OF FRIES','🍟',0,20,10,GenericFood])
+FoodItemList.append(['SLICE OF BACON','🥓',0,40,15,Bacon])
 FoodItemList.append(['TURKEY LEG','🍗',0,30,15,GenericFood])
 FoodItemList.append(['SODA','🥤',0,20,7,Drink])
-FoodItemList.append(['PIZZA','🍕',0,35,7,Pizza])
+FoodItemList.append(['SLICE OF PIZZA','🍕',0,35,7,Pizza])
 FoodItemList.append(['TACO','🌮',0,25,7,GenericFood])
-FoodItemList.append(['CHEESE','🧀',0,20,7,Cheese])
-FoodItemList.append(['ASIAN NOODLE','🍜',0,30,10,WokSpoon])
+FoodItemList.append(['WEDGE OF CHEESE','🧀',0,20,7,Cheese])
+FoodItemList.append(['CARTON OF ASIAN NOODLE','🍜',0,30,10,WokSpoon])
 FoodItemList.append(['STAKE','🥩',0,40,15,GenericFood])
 
 def SetupPlayerStartItems():
